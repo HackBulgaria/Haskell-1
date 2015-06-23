60c74667aa687e9b7d309aa34a6d68085ba64eff
leohaskell <daniel.taskoff@gmail.com>
2015-06-23 17:11:59 +0300

    Removing a test .hs file from the materials

--- week2/main.hs
+++
@@ -1,9 +0,0 @@
-{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
-{-# LANGUAGE BangPatterns #-}
-
-length' (_:xs) = 1 + length' xs
-length' _      = 0
-
-length'' xs = helper xs 0
-  where helper (_:xs) !n = helper xs (n + 1)
-        helper _      !n = n 

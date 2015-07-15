b76527cd97f53af5ff0b3ae9ace90a565c36c28d
leohaskell <daniel.taskoff@gmail.com>
2015-07-15 18:01:15 +0300

    Adding missing length function

--- week3/4-DataTypes/solutions/List.hs
+++ week3/4-DataTypes/solutions/List.hs
@@ -30,9 +30,26 @@ filterList p (Cons x xs) = if p x
 
 foldlList :: (a -> b -> a) -> a -> List b -> a
 foldlList f z (Cons x xs) = foldlList f (f z x) xs
+foldlList _ z _           = z
 
 foldrList :: (a -> b -> b) -> b -> List a -> b
 foldrList f z (Cons x xs) = f x $ foldrList f z xs
 foldrList _ z _           = z
+
+lengthList :: List a -> Int
+lengthList = foldlList (\n -> (+1) . const n) 0
+
+safeHead :: List a -> Maybe a
+safeHead (Cons x _) = Just x
+safeHead _          = Nothing
+
+safeTail :: List a -> Maybe (List a)
+safeTail (Cons _ xs) = Just xs
+safeTail _           = Nothing
+
+safeNth :: Int -> List a -> Maybe a
+safeNth 0 (Cons x _)  = Just x
+safeNth n (Cons _ xs) = safeNth (n-1) xs
+safeNth _ _           = Nothing
 
 type IntList = List Int
